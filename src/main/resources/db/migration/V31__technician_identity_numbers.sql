0cc89d7ab4b7c75a4a7d21eb9ec72f9b0b026caf
Gopi1603 <gopichowdary1603@gmail.com>
2026-09-22 17:45:13 +0530

    fix: make technician identity migration portable

--- src/main/resources/db/migration/V31__technician_identity_numbers.sql
+++ src/main/resources/db/migration/V31__technician_identity_numbers.sql
@@ -1,3 +1,2 @@
-alter table technician_applications
-  add column aadhaar_number varchar(20) null,
-  add column driving_license_number varchar(40) null;
+alter table technician_applications add column aadhaar_number varchar(20);
+alter table technician_applications add column driving_license_number varchar(40);
